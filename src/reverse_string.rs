aacafeb1a916496a8c009cfd2fed35d6d2a236d9
sebmaz <seb.maz93@icloud.com>
2023-06-09 03:10:47 +0400

    reverse a string

--- src/reverse_string.rs
+++ src/reverse_string.rs
@@ -2,6 +2,9 @@ fn reverse(input: &str) -> String {
     let mut temp = input.chars().collect::<Vec<_>>();
     temp.reverse();
     temp.iter().collect::<String>()
+
+    // --- short solution ---
+    // input.chars().rev().collect()
 }
 
 #[test]
